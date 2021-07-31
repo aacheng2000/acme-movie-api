787562ce3d44497a1f04f73c2c80bf588595ef0f
EC2 Default User <ec2-user@ip-172-31-38-153.us-east-2.compute.internal>
2021-07-31 14:11:49 +0000

    first commit

---
+++ test/spec.js
@@ -0,0 +1,9 @@
+const {expect} = require('chai')
+
+describe('User.byToken', ()=>{
+        describe('with a valid token', ()=> {
+            it('returns a user', async()=> {
+                expect(1).to.equal(1)
+            })
+        })
+    })
