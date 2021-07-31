502972bfcf8d394494285f77d4e1282a8ecba4f3
EC2 Default User <ec2-user@ip-172-31-38-153.us-east-2.compute.internal>
2021-07-31 15:02:53 +0000

    fifth commit

--- test/spec.js
+++ test/spec.js
@@ -13,15 +13,11 @@ const express = require('express');
 
 const app = express();
 
-app.get('/user', function(req, res) {
-  res.status(200).json({ name: 'john' });
-});
-
-request(app)
-  .get('/user')
-  .expect('Content-Type', /json/)
-  .expect('Content-Length', '15')
-  .expect(200)
-  .end(function(err, res) {
-    if (err) throw err;
-  });
+describe('Routes', ()=> {
+    describe('get /', ()=> {
+        it('show information about the api', async() => {
+            const response = await app.get('/');
+            expect(response.status).to.equal(200);
+        })
+    })
+})
